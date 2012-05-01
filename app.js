ae5903049587e3d9e09ba41f5cc686844932ec15
Brian Hazzard <bhazzard@gmail.com>
2012-05-01 04:37:02 +0000

    html approach to rest

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 var express = require('express'),
-    app = express.createServer();
+    app = express.createServer(express.bodyParser());
 
 /* lets do a simple twitter clone */
 var User = require('./models/user');
 app.get('/register', function(req, res) {
-
+    res.render('registration.ejs');
 });
 
 app.post('/register', function(req, res) {
-    var u = new User();
-    u.save();
-    res.send();
+    var user = new User();
+    user.username = req.body.username;
+    user.password = req.body.password;
+    user.save(function(err) {
+        if (err) return res.send(err, 400);
+        
+        res.header('Location', '/users/' + user._id);
+        res.statusCode(201);
+        res.render('user/show.ejs', user);
+    });
 });
 
 app.post('/message', function(req, res) {
@@ -18,7 +25,19 @@ app.post('/message', function(req, res) {
 });
 
 app.get('/users/:id', function(req, res) {
-    
+    User.findOne({_id: req.params.id}, function(err, user) {
+       if (err) return res.send(err, 404);
+       
+       res.render('user/show.ejs', user);
+    });
+});
+
+app.post('/users/:id', function(req, res) {
+    User.update({_id: req.params.id}, req.body, function(err, user) {
+       if (err) return res.send(err, 404);
+       
+       res.render('user/show.ejs', 202);
+    });
 });
 
 app.get('/users/:id/followers', function(req, res) {
--- models/user.js
+++ models/user.js
@@ -1,8 +1,8 @@
 var mongoose = require('./my_mongoose'),
 
 UserSchema = new mongoose.Schema({
-    username: {type: String, default: "blue"},
-    password: {type: String, default: "man"}
+    username: {type: String, required: true },
+    password: {type: String, required: true }
 }),
 
 User = mongoose.model('User', UserSchema);
